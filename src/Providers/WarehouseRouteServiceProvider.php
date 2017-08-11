9d6b5af11436d264b7639b366cfe50c0d6ac7680
domee1337 <a2@fs24.local>
2017-08-11 15:32:07 +0200

    lite

--- src/Providers/WarehouseRouteServiceProvider.php
+++ src/Providers/WarehouseRouteServiceProvider.php
@@ -13,11 +13,11 @@ class WarehouseRouteServiceProvider extends RouteServiceProvider{
         /**
         * GET Routing Templates
         */
-        $router->get('storage',                         'Storage24Lite\Controllers\ContentController@render_start');
-        $router->get('storage/transfer',              'Storage24Lite\Controllers\ContentController@render_transfer');
-        $router->get('storage/inventur',              'Storage24Lite\Controllers\ContentController@render_inventur');
-        $router->get('storage/test',                  'Storage24Lite\Controllers\ContentController@test');
-        $router->get('storage/incoming',                  'Storage24Lite\Controllers\ContentController@render_incoming');
+        $router->get('storagelite',                         'Storage24Lite\Controllers\ContentController@render_start');
+        $router->get('storagelite/transfer',              'Storage24Lite\Controllers\ContentController@render_transfer');
+        $router->get('storagelite/inventur',              'Storage24Lite\Controllers\ContentController@render_inventur');
+        $router->get('storagelite/test',                  'Storage24Lite\Controllers\ContentController@test');
+        $router->get('storagelite/incoming',                  'Storage24Lite\Controllers\ContentController@render_incoming');
     }
 
 }
